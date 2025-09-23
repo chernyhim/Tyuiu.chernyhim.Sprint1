9d7dfbe3b5038de18960bc1bad28d53b92225eb0
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-09-23 19:14:54 +0500

    Task3

---
+++ Tyuiu.chernyhim.Sprint1.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.chernyhim.Sprint1.Task3.V19.Lib;
+namespace Tyuiu.chernyhim.Sprint1.Task3.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 2;
+            double x2 = 4;
+            double y1 = 3;
+            double y2 = 1;
+            var res = ds.ElephCanMove(x1, x2, y1, y2);
+            Assert.AreEqual(res, true);
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint1.Task3.V19/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.chernyhim.Sprint1.Task3.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
