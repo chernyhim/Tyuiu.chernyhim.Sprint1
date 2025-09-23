debfa28986bfcecb89e67f887be2b64846a2bc38
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-09-23 17:57:37 +0500

    Task2

---
+++ Tyuiu.chernyhim.Sprint1.Task1.V7.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.chernyhim.Sprint1.Task1.V7.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint1.Task2.V7/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.chernyhim.Sprint1.Task2.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Sprint #1 | Made by Chernyh I. M. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Черных Илья Максимович | ПКТб-25-1                            *");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Площадь круга " + ds.CalculateSquareCircle(x));
+        Console.ReadLine();
+    }
+}
